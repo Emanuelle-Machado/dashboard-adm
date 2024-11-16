a797df89ac849425c7fad85896663f89eac68e84
Emanuelle Machado <manumachado03@outlook.com>
2024-11-16 00:03:04 -0300

    projeto começado, com mudança de temas e inicio de side bar

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,51 @@
+import { Box, IconButton, Typhography, useTheme } from "@mui/material";
+import { useState } from "react";
+import { Prosidebar, Menu, MenuItem } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimeOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+const Sidebar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.ode);
+    const [isCollpased, setIsCollpased] = useState(false);
+    const [selected, setSelected] = useState('Dashboard');
+
+    return (
+        <Box
+            sx={{
+                "& .pro-sidebar-inner": {
+                    backgroundColor: `${colors.primary[400]} !important`
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important"
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important"
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important"
+                },
+                "& .pro-menu-item.active": {
+                    color: "#6870fa !important"
+                }
+            }}
+        >
+
+        </Box>
+    )
+}
+
+export default Sidebar
